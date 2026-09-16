4f9a2e8022132c1b966830e4478cdc8aa7664009
KuroKumono <kurokumono@gmail.com>
2026-09-16 20:28:38 +0900

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+// 
+window.SOUL_SITE_CONFIG = {
+  supabaseUrl: 'https://kuduuscsrngkmjwjesdk.supabase.co',
+  supabasePublishableKey: 'sb_publishable_2VyDyLML09vfTklxJiiBvw_cJgHaGRF',
+  adminEmail: 'kurokumono@gmail.com'
+};
